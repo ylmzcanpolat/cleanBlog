3ecccca459f72bb96a47e90637cf9e2bd922cd02
ylmzcanpolat <71183671+ylmzcanpolat@users.noreply.github.com>
2021-06-03 00:34:06 +0300

    workspace created

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express');
-
+const ejs = require('ejs');
 const app = express();
 
-app.get('/', (req, res) => {
-  const blog = {
-    id: 1,
-    title: 'Blog title',
-    description: 'Blog description',
-  };
+app.set('view engine', 'ejs');
 
-  res.send(blog);
+
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+  res.render("index");
+});
+
+app.get('/about', (req, res) => {
+  res.render("about");
+});
+
+app.get('/add_post', (req, res) => {
+  res.render("add_post");
+});
+
+app.get('/post', (req, res) => {
+  res.render("post");
 });
 
 const port = 3000;
