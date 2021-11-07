5e16fa6e3222410e3ea6b838b50d49d6a7bfbf7f
ylmzcanpolat <71183671+ylmzcanpolat@users.noreply.github.com>
2021-11-07 17:06:01 +0300

    last

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const ejs = require('ejs');
-const path = require('path');
-const Post = require('./models/Post');
+const postController = require("./controllers/postControllers")
+const pageController = require("./controllers/pageControllers")
+
 
 const app = express();
 
 mongoose.connect('mongodb://localhost/cleanblog-test-db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
 app.set('view engine', 'ejs');
@@ -17,38 +20,21 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method', {
+  methods: ["POST", "GET"]
+}));
 
-app.get('/', async (req, res) => {
-  const posts = await Post.find({});
+//Routes
 
-  res.render("index", {
-    posts : posts
-  });
-});
+app.get('/', postController.getAllPosts);
+app.get('/posts/:id', postController.getPost);
+app.post("/posts", postController.createPost);
+app.put("/posts/:id", postController.updatePost);
+app.delete("/posts/:id", postController.deletePost);
 
-app.get('/posts/:id', async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.render("post", {
-    post
-  });
-});
-
-app.get('/about', (req, res) => {
-  res.render("about");
-});
-
-app.get('/add_post', (req, res) => {
-  res.render("add_post");
-});
-
-app.get('/post', (req, res) => {
-  res.render("post");
-});
-
-app.post("/posts", async (req, res)=>{
-  await Post.create(req.body);
-  res.redirect("/");
-});
+app.get('/about', pageController.getAboutPage);
+app.get('/add_post', pageController.getAddPage);
+app.get('/posts/edit/:id', pageController.getEditPage);
 
 const port = 3000;
 
