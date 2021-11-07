d4fda6809481851ae03229c75a09b227a02d94bd
ylmzcanpolat <71183671+ylmzcanpolat@users.noreply.github.com>
2021-11-07 22:41:07 +0300

    Reday for heroku

--- app.js
+++ app.js
@@ -1,42 +1,51 @@
+// core modules
 const express = require('express');
 const mongoose = require('mongoose');
+
+// 3 th party modules
 const methodOverride = require('method-override');
 const ejs = require('ejs');
+
+//local modules
 const postController = require("./controllers/postControllers")
 const pageController = require("./controllers/pageControllers")
 
-
 const app = express();
 
-mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+//connect DB
+mongoose.connect('mongodb+srv://yilmaz:x-Jk%23WEuF3ihWs5@cluster0.htdwu.mongodb.net/cleanblog-db?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
+}).then(() => {
+  console.log("CONNECTED TO DB...")
+}).catch((err) => {
+  console.log(err)
 });
 
+//temlate engine
 app.set('view engine', 'ejs');
 
-
+//Middlewares
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(methodOverride('_method', {
   methods: ["POST", "GET"]
 }));
 
 //Routes
-
 app.get('/', postController.getAllPosts);
 app.get('/posts/:id', postController.getPost);
 app.post("/posts", postController.createPost);
 app.put("/posts/:id", postController.updatePost);
 app.delete("/posts/:id", postController.deletePost);
-
 app.get('/about', pageController.getAboutPage);
 app.get('/add_post', pageController.getAddPage);
 app.get('/posts/edit/:id', pageController.getEditPage);
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`sunucu ${port} numaralı portta başlatıldı..`);
--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,13 +1,27 @@
 const Post = require("../models/Post")
 
+//Veri tabanından tüm postları almak için.
 exports.getAllPosts = async (req, res) => {
-    const posts = await Post.find({}).sort("-dateCreated");
+    //Pagination
+    const page = req.query.page || 1;
+    const postPerPage = 2;
+
+    const totalPost = await Post.find().countDocuments();
+
+    const posts = await Post.find({})
+        .sort("-dateCreated")
+        .skip((page - 1) * postPerPage)
+        .limit(postPerPage)
 
     res.render("index", {
-        posts: posts
+        posts: posts,
+        current: page,
+        pages: Math.ceil(totalPost / postPerPage)
     });
+
 };
 
+//Veri tabanından post almak için.
 exports.getPost = async (req, res) => {
     const post = await Post.findById(req.params.id);
     res.render("post", {
@@ -15,11 +29,13 @@ exports.getPost = async (req, res) => {
     });
 }
 
+//Veri tabanında yeni post oluşturmak için.
 exports.createPost = async (req, res) => {
     await Post.create(req.body);
     res.redirect("/");
 }
 
+//Veri tabanında post güncellemek için.
 exports.updatePost = async (req, res) => {
     const post = await Post.findOne({ _id: req.params.id })
     post.title = req.body.title
@@ -29,6 +45,7 @@ exports.updatePost = async (req, res) => {
     res.redirect(`/posts/${req.params.id}`)
 };
 
+//Veri tabanından post silmek için.
 exports.deletePost = async (req, res) => {
     await Post.findByIdAndRemove(req.params.id)
     res.redirect("/")
